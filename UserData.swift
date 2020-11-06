e6b099e4c0b1384824dc310efe5a7beb6681e364
rlt202 <danik.nikulin@gmail.com>
2020-11-06 15:50:18 +0300

    User Data and authorization view

---
+++ UserData.swift
@@ -0,0 +1,17 @@
+//
+//  UserData.swift
+//  solarSystem
+//
+//  Created by Даниил Никулин on 06.11.2020.
+//
+
+import Foundation
+
+struct User {
+    let username: String
+    let password: String
+    
+    static func getData() -> User {
+        User(username: "Solar", password: "System")
+    }
+}
