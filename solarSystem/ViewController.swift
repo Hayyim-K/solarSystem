0a31f08f5ef928f5012c422a53e3d880f8ab00ef
Hayyim <b2banalytica@gmail.com>
2020-11-06 11:10:45 +0300

    Initial Commit

---
+++ solarSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  solarSystem
+//
+//  Created by vitasiy on 06.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
